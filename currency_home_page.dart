a38e87744ce87ed02653d6b83e5d1e1a183fb05f
musamiumar <77604263+musamiumar@users.noreply.github.com>
2024-03-09 15:01:55 +0200

    Add files via upload

---
+++ currency_home_page.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+
+class CurrencyHomePage extends StatefulWidget {
+  const CurrencyHomePage({super.key});
+  @override
+  State<CurrencyHomePage> createState() => _CurrencyHomePageState();
+}
+
+class _CurrencyHomePageState extends State<CurrencyHomePage> {
+  double result = 0;
+  TextEditingController textEditingController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    const border = OutlineInputBorder(
+      borderSide: BorderSide(
+        color: Colors.black,
+        width: 1,
+        style: BorderStyle.solid,
+      ),
+      borderRadius: BorderRadius.all(Radius.circular(5)),
+    );
+
+    return Scaffold(
+      backgroundColor: Colors.blueGrey,
+      appBar: AppBar(
+        backgroundColor: Colors.blueGrey,
+        title: const Text('Currency converter'),
+        elevation: 0,
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Naira ' '${result}',
+              style: const TextStyle(
+                  fontSize: 55,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: TextField(
+                controller: textEditingController,
+                decoration: const InputDecoration(
+                    hintText: 'Please Enter amount in USD',
+                    hintStyle: TextStyle(color: Colors.black),
+                    prefixIcon: Icon(Icons.monetization_on_outlined),
+                    prefixIconColor: Colors.black,
+                    filled: true,
+                    fillColor: Colors.white,
+                    focusedBorder: border,
+                    enabledBorder: border),
+                keyboardType:
+                    const TextInputType.numberWithOptions(decimal: true),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    result =
+                        (double.parse(textEditingController.text) * 1580.88);
+                  });
+                },
+                style: TextButton.styleFrom(
+                    backgroundColor: Colors.black,
+                    foregroundColor: Colors.white,
+                    minimumSize: const Size(double.infinity, 50),
+                    shape: (RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10)))),
+                child: const Text('Convert'),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
